da49d3a3550dc1209e1e1e4189485a85d596d9ff
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-02-17 15:23:22 -0500

    CORE-963: extract marc records loading in test in setup_all

--- test/marc_record_ex_test.exs
+++ test/marc_record_ex_test.exs
@@ -2,24 +2,27 @@ defmodule MarcRecordExTest do
   use ExUnit.Case
   doctest MarcRecordEx
 
+  setup_all do
+    records =
+      File.read!("test/fixtures/record.mrc")
+      |> MarcRecordEx.parse_records_wrapper()
+
+    {:ok, records: records}
+  end
+
   test "parser return error for invalid data" do
     content = <<"aa"::binary>>
     {status, _} = MarcRecordEx.parse_records_wrapper(content)
     assert status == :error
   end
 
-  test "parser return 109 records from record.mrc" do
-    records =
-      File.read!("test/fixtures/record.mrc")
-      |> MarcRecordEx.parse_records_wrapper()
-
-    assert length(records) == 109
+  test "parser return 109 records from record.mrc", state do
+    assert length(state[:records]) == 109
   end
 
-  test "record 1 return 30 fields" do
+  test "record 1 return 30 fields", state do
     fields =
-      File.read!("test/fixtures/record.mrc")
-      |> MarcRecordEx.parse_records_wrapper()
+      state[:records]
       |> Enum.at(0)
       |> Map.get("fields")
 
