4a21a2b147b220cd9fbd32815a80f1b7871b60d2
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-02-17 09:29:08 -0500

    CORE-963: add test to check record quantity

--- test/marc_record_ex_test.exs
+++ test/marc_record_ex_test.exs
@@ -7,4 +7,12 @@ defmodule MarcRecordExTest do
     {status, _} = MarcRecordEx.parse_records_wrapper(content)
     assert status == :error
   end
+
+  test "parser return a list of all the records" do
+    records =
+      File.read!("test/fixtures/record.mrc")
+      |> MarcRecordEx.parse_records_wrapper()
+
+    assert length(records) == 109
+  end
 end
