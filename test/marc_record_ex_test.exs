9b957344372d0bd16dcb541426ba8921e98e34db
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-02-17 12:03:49 -0500

    CORE-963: test how many subfields for record 1

--- test/marc_record_ex_test.exs
+++ test/marc_record_ex_test.exs
@@ -8,11 +8,21 @@ defmodule MarcRecordExTest do
     assert status == :error
   end
 
-  test "parser return a list of all the records" do
+  test "parser return 109 records from record.mrc" do
     records =
       File.read!("test/fixtures/record.mrc")
       |> MarcRecordEx.parse_records_wrapper()
 
     assert length(records) == 109
+  end
+
+  test "record 1 return 30 fields" do
+    fields =
+      File.read!("test/fixtures/record.mrc")
+      |> MarcRecordEx.parse_records_wrapper()
+      |> Enum.at(0)
+      |> Map.get("fields")
+
+    assert length(fields) == 30
   end
 end
