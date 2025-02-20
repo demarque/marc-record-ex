fab6998eaa4277c937063ae1c2d148cd8b9ad257
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-02-20 13:38:35 -0500

    Bump elixir version to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MarcRecordEx.MixProject do
     [
       app: :marc_record_ex,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
